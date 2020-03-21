6aed50e4fc60e16e397467793fdbef832f3145a5
Nisaachar <thhat.guy.is@gmail.com>
2020-03-21 20:11:37 +0530

    Fixed the issues with fails

--- script.py
+++ script.py
@@ -1,9 +1,12 @@
 import openpyxl
 
+# Name of the file. Keep this script and file in same folder.
+filename = 'test'
+
 #variables for excell sheet
 starting_rows = 13
 starting_column = 12
-max_entries = 17
+no_of_candidates = 17
 max_attributes = 26
 
 #marks 
@@ -12,7 +15,7 @@ drill_pr_total = 80
 drill_total = 90
 
 writing_wr_total = 35
-writing_pr_total = 60
+writing_pr_total = 25
 writing_total = 60
 
 misc_total = 200
@@ -24,18 +27,19 @@ special_total = 150
 total = 500
 
 pass_ind = 0.45
-pass_ttl= 0.5
+pass_ttl= 0.50
 
 
-wb = openpyxl.load_workbook('test.xlsx')
+filename = filename + '.xlsx'
+wb = openpyxl.load_workbook(filename)
 sheet = wb["Sheet1"]
 sheet = wb.active
 
 
-# for value in sheet.iter_rows(min_row=starting_rows, max_row= max_entries, min_col=starting_column, max_col=max_attributes, values_only=True):
+# for value in sheet.iter_rows(min_row=starting_rows, max_row= no_of_candidates, min_col=starting_column, max_col=max_attributes, values_only=True):
 #     print(value)
 
-for rows in range(starting_rows, max_entries):
+for rows in range(starting_rows, no_of_candidates):
 
     drill_tr = 0
     writing_tr = 0
@@ -43,54 +47,101 @@ for rows in range(starting_rows, max_entries):
     spl_tr = 0
     grand_total = 0
     count = 1
-
+    is_pass = 1
     for columns in range(starting_column, max_attributes):
         if(count == 1):
             drill_wr = int(sheet.cell(row = rows, column = columns).value)
+        
         elif(count == 2):
             drill_pr = int(sheet.cell(row = rows, column = columns).value)
+        
         elif(count == 3):
             drill_tr =  int(sheet.cell(row = rows, column = columns).value)
+            
         elif(count == 4):
             writing_wr = int(sheet.cell(row = rows, column = columns).value)
+            
         elif(count == 5):
             writing_pr = int(sheet.cell(row = rows, column = columns).value)
+            
         elif(count == 6):
             writing_tr = int(sheet.cell(row = rows, column = columns).value)
+            
         elif(count == 7):
             misc_tr = int(sheet.cell(row = rows, column = columns).value)
+            
         elif(count == 8):
             special_wr = int(sheet.cell(row = rows, column = columns).value)
+            
         elif(count == 9):
             special_pr = int(sheet.cell(row = rows, column = columns).value)
+            
         elif(count == 10):
             spl_tr = int(sheet.cell(row = rows, column = columns).value)
+            
         elif(count == 13):
             grand_total = int(sheet.cell(row = rows, column = columns).value)
         elif(count == 14):
-            if(drill_wr > drill_wr_total * pass_ind) and(drill_pr > drill_pr_total * pass_ind) and (drill_tr > drill_total * pass_ind) and (writing_wr > writing_wr_total * pass_ind ) and (writing_pr > writing_pr_total * pass_ind) and (writing_tr > writing_total * pass_ind) and (misc_tr > misc_total* pass_ind) and (special_wr > special_wr_total * pass_ind) and (special_pr > special_pr_total * pass_ind) and (spl_tr > special_total* pass_ind) and (grand_total > total * pass_ttl):
+            if(drill_wr > drill_wr_total * pass_ind) and (drill_pr > drill_pr_total * pass_ind) and (drill_tr > drill_total * pass_ind) and (writing_wr > writing_wr_total * pass_ind) and (writing_pr > writing_pr_total * pass_ind) and (writing_tr > writing_total * pass_ind) and (misc_tr > misc_total* pass_ind) and (special_wr > special_wr_total * pass_ind) and (special_pr > special_pr_total * pass_ind) and (spl_tr > special_total* pass_ind) and (grand_total > total * pass_ttl):
                 if(grand_total > 375):
-                    # temp = sheet.cell(row = rows, column = max_attributes)
-                    # temp.value = 'A'
-                    var = 'Y' + str(rows)
-                    sheet[var] = "A"
+                    temp = sheet.cell(row = rows, column = max_attributes-1)
+                    temp.value = 'A'
+                    # var = 'Y' + str(rows)
+                    # sheet[var] = "A"
                 if(grand_total > 300) and (grand_total < 374):
-                    # temp = sheet.cell(row = rows, column = max_attributes)
-                    # temp.value = 'B'
-                    var = 'Y' + str(rows)
-                    sheet[var] = "B"
+                    temp = sheet.cell(row = rows, column = max_attributes-1)
+                    temp.value = 'B'
+                    # var = 'Y' + str(rows)
+                    # sheet[var] = "B"
                 if(grand_total > 250) and (grand_total < 299):
-                    # temp = sheet.cell(row = rows, column = max_attributes)
-                    # temp.value = 'C'
-                    var = 'Y' + str(rows)
-                    sheet[var] = "C"
+                    temp = sheet.cell(row = rows, column = max_attributes-1)
+                    temp.value = 'C'
+                    # var = 'Y' + str(rows)
+                    # sheet[var] = "C"
             else:
-                # temp = sheet.cell(row = rows, column = max_attributes)
-                # temp.value = 'Fail'
-                var = 'Y' + str(rows)
-                sheet[var] = "Fail"
-        else:
-            b = 2
+                temp = sheet.cell(row = rows, column = max_attributes-1)
+                temp.value = 'Fail'
+                # var = 'Y' + str(rows)
+                # sheet[var] = "Fail"
+            # if(drill_wr > drill_wr_total * pass_ind):
+            #     if(drill_pr > drill_pr_total * pass_ind):
+            #         if(drill_tr > drill_total * pass_ind):
+            #             if(writing_wr > writing_wr_total * pass_ind):
+            #                 if(writing_pr > writing_pr_total * pass_ind):
+            #                     if(writing_tr > writing_total * pass_ind):
+            #                         if(misc_tr > misc_total * pass_ind):
+            #                             if(special_wr > special_wr_total * pass_ind):
+            #                                 if(special_pr > special_pr_total * pass_ind):
+            #                                     if(spl_tr > special_total * pass_ind):
+            #                                         if(grand_total > total * pass_ttl):
+            #                                             is_pass = 1
+            # if(is_pass == 1):
+            #     if(grand_total > 375):
+            #         # temp = sheet.cell(row = rows, column = max_attributes)
+            #         # temp.value = 'A'
+            #         var = 'Y' + str(rows)
+            #         sheet[var] = "A"
+            #     elif(grand_total > 300) and (grand_total < 374):
+            #         # temp = sheet.cell(row = rows, column = max_attributes)
+            #         # temp.value = 'B'
+            #         var = 'Y' + str(rows)
+            #         sheet[var] = "B"
+            #     elif(grand_total > 250) and (grand_total < 299):
+            #         # temp = sheet.cell(row = rows, column = max_attributes)
+            #         # temp.value = 'C'
+            #         var = 'Y' + str(rows)
+            #         sheet[var] = "C"
+            #     else:
+            #     # temp = sheet.cell(row = rows, column = max_attributes)
+            #     # temp.value = 'Fail'
+            #         var = 'Y' + str(rows)
+            #         sheet[var] = "Fail"
+            # else:
+            #     # temp = sheet.cell(row = rows, column = max_attributes)
+            #     # temp.value = 'Fail'
+            #     var = 'Y' + str(rows)
+            #     sheet[var] = "Fail"
+
         count = count + 1
 
         # if(columns >= starting_column + 0 & columns < starting_column + 2):
